acb1feb5c51f16fbd522443df2ce5bc9ee48d679
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-06-06 10:25:27 -0300

    import React

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+/* eslint-disable */
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 
 export default function App() {
   return <StatusBar style="auto" />;
