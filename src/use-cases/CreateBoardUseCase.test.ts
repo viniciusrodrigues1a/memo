b2ab410cb0f1235f1f6ab3702d812bcd84c85395
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-06-07 05:06:33 -0300

    create factory to instantiate sut

--- src/use-cases/CreateBoardUseCase.test.ts
+++ src/use-cases/CreateBoardUseCase.test.ts
@@ -9,54 +9,50 @@ import {
   BoardNameExceedsMaximumLength,
 } from "./errors";
 
+function makeSut() {
+  const inMemoryCreateBoardRepository = new InMemoryCreateBoardRepository();
+  const inMemoryShowBoardRepository = new InMemoryShowBoardRepository();
+  const sut = new CreateBoardUseCase(
+    inMemoryCreateBoardRepository,
+    inMemoryShowBoardRepository
+  );
+
+  return { inMemoryShowBoardRepository, inMemoryCreateBoardRepository, sut };
+}
+
 describe("Board creation use-case", () => {
   afterEach(() => {
     inMemoryHelperArray.splice(0, inMemoryHelperArray.length);
   });
 
   it("should be able to create a new Board", () => {
-    const inMemoryCreateBoardRepository = new InMemoryCreateBoardRepository();
-    const inMemoryShowBoardRepository = new InMemoryShowBoardRepository();
-    const createBoardUseCase = new CreateBoardUseCase(
-      inMemoryCreateBoardRepository,
-      inMemoryShowBoardRepository
-    );
+    const { sut } = makeSut();
 
-    createBoardUseCase.create("My new board");
+    sut.create("My new board");
 
     const board = inMemoryHelperArray.find((b) => b.name === "My new board");
     expect(board?.name).toEqual("My new board");
   });
 
   it("should not be able to create a new Board because there is a Board with that name already", () => {
-    const inMemoryCreateBoardRepository = new InMemoryCreateBoardRepository();
-    const inMemoryShowBoardRepository = new InMemoryShowBoardRepository();
-    const createBoardUseCase = new CreateBoardUseCase(
-      inMemoryCreateBoardRepository,
-      inMemoryShowBoardRepository
-    );
+    const { sut, inMemoryShowBoardRepository } = makeSut();
 
     inMemoryShowBoardRepository.show = jest.fn(() => ({
       name: "My new board",
     }));
 
-    expect(() => createBoardUseCase.create("My new board")).toThrow(
+    expect(() => sut.create("My new board")).toThrow(
       new BoardAlreadyExistsError("My new board")
     );
   });
 
   it("should not be able to create a new Board if its name is longer than 32 characters", () => {
-    const inMemoryCreateBoardRepository = new InMemoryCreateBoardRepository();
-    const inMemoryShowBoardRepository = new InMemoryShowBoardRepository();
-    const createBoardUseCase = new CreateBoardUseCase(
-      inMemoryCreateBoardRepository,
-      inMemoryShowBoardRepository
-    );
+    const { sut, inMemoryShowBoardRepository } = makeSut();
 
     inMemoryShowBoardRepository.show = jest.fn(() => undefined);
 
-    expect(() =>
-      createBoardUseCase.create("My new board with a very long name")
-    ).toThrow(new BoardNameExceedsMaximumLength(32));
+    expect(() => sut.create("My new board with a very long name")).toThrow(
+      new BoardNameExceedsMaximumLength(32)
+    );
   });
 });
