a9360d18cf4a0ac1d549bf98ff8f8097800df634
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-06-10 09:02:39 -0300

    add crypto.getRandomValues() polyfill to satisfy uuidjs

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -1,4 +1,5 @@
 import "react-native-gesture-handler";
+import "react-native-get-random-values";
 import { registerRootComponent } from "expo";
 import React from "react";
 import StatusBar from "./components/StatusBar";
