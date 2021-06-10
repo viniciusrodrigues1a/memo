ea095d1210c0756c16d871b8850ebf55878006ac
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-06-10 04:54:32 -0300

    persist Boards

--- src/infra/repositories/AsyncStorageCreateBoardRepository.test.ts
+++ src/infra/repositories/AsyncStorageCreateBoardRepository.test.ts
@@ -1,10 +1,24 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AsyncStorageCreateBoardRepository } from "./AsyncStorageCreateBoardRepository";
 import { Board } from "../../entities";
+import { IListBoardRepository } from "../../use-cases/interfaces";
+
+class MockListBoardRepository implements IListBoardRepository {
+  async list(): Promise<Board[]> {
+    return [];
+  }
+}
+
+function makeSut() {
+  const mockListBoardRepository = new MockListBoardRepository();
+  const sut = new AsyncStorageCreateBoardRepository(mockListBoardRepository);
+
+  return { sut, mockListBoardRepository };
+}
 
 describe("Board creation using AsyncStorage", () => {
   it("should be able to store a Board", async () => {
-    const sut = new AsyncStorageCreateBoardRepository();
+    const { sut } = makeSut();
 
     const board = await sut.create("My new board");
 
@@ -14,5 +28,36 @@ describe("Board creation using AsyncStorage", () => {
 
     const storedBoard = boards.find((b: Board) => b.name === board.name);
     expect(storedBoard.name).toEqual(board.name);
+  });
+
+  it("should NOT remove other Boards when adding a new one", async () => {
+    const { sut, mockListBoardRepository } = makeSut();
+
+    const boardId = "85d08a66-1e16-4cb6-879f-4339b7d46b9a";
+    const olderBoard: Board = {
+      id: boardId,
+      name: "My first board",
+      statuses: [
+        {
+          name: "todo",
+          boardId,
+          colorHex: "#FF3300",
+          stories: [],
+        },
+      ],
+    };
+
+    mockListBoardRepository.list = jest.fn(
+      async (): Promise<Board[]> => [olderBoard]
+    );
+
+    await sut.create("My second board");
+
+    const boards = JSON.parse(
+      (await AsyncStorage.getItem("@Memo:boards")) as string
+    );
+
+    const storedBoard = boards.find((b: Board) => b.name === olderBoard.name);
+    expect(storedBoard.name).toEqual(olderBoard.name);
   });
 });
--- src/infra/repositories/AsyncStorageCreateBoardRepository.ts
+++ src/infra/repositories/AsyncStorageCreateBoardRepository.ts
@@ -1,11 +1,16 @@
 import { v4 as uuid } from "uuid";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Board } from "../../entities";
-import { ICreateBoardRepository } from "../../use-cases/interfaces";
+import {
+  ICreateBoardRepository,
+  IListBoardRepository,
+} from "../../use-cases/interfaces";
 
 export class AsyncStorageCreateBoardRepository
   implements ICreateBoardRepository
 {
+  constructor(private listBoardRepository: IListBoardRepository) {}
+
   async create(name: string): Promise<Board> {
     const boardId = uuid();
     const board: Board = {
@@ -33,7 +38,9 @@ export class AsyncStorageCreateBoardRepository
       ],
     };
 
-    await AsyncStorage.setItem("@Memo:boards", JSON.stringify([board]));
+    const repositories = await this.listBoardRepository.list();
+    const boards = repositories.concat([board]);
+    await AsyncStorage.setItem("@Memo:boards", JSON.stringify(boards));
 
     return board;
   }
