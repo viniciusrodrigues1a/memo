775abde38b258c08dac711d7b460ac59083f2ae1
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-06-10 05:05:05 -0300

    return empty array if key is null in AsyncStorage

--- src/infra/repositories/AsyncStorageListBoardRepository.test.ts
+++ src/infra/repositories/AsyncStorageListBoardRepository.test.ts
@@ -5,12 +5,22 @@ import { IListBoardRepository } from "../../use-cases/interfaces";
 class AsyncStorageListBoardRepository implements IListBoardRepository {
   async list(): Promise<Board[]> {
     const storedBoards = await AsyncStorage.getItem("@Memo:boards");
-    const boards = JSON.parse(storedBoards as string);
+
+    if (!storedBoards) {
+      return [];
+    }
+
+    const boards = JSON.parse(storedBoards);
+
     return boards;
   }
 }
 
 describe("List all Boards using AsyncStorage", () => {
+  afterEach(async () => {
+    await AsyncStorage.removeItem("@Memo:boards");
+  });
+
   it("should be able to list stored Boards", async () => {
     const sut = new AsyncStorageListBoardRepository();
     await AsyncStorage.setItem(
@@ -35,5 +45,13 @@ describe("List all Boards using AsyncStorage", () => {
 
     const boardsNames = boards.map((b) => b.name);
     expect(boardsNames).toContain("My board");
+  });
+
+  it("should return empty array if there is no Board stored", async () => {
+    const sut = new AsyncStorageListBoardRepository();
+
+    const boards = await sut.list();
+
+    expect(boards.length).toBe(0);
   });
 });
